5c461d9ac6127fa6727b9438d13b149d571e2ef7
Marco Meini <marco@marcomeini.it>
2022-08-24 09:58:40 +0200

    query esordio fantini

--- _db/query.sql
+++ _db/query.sql
@@ -36,3 +36,11 @@ inner join fantini c on a.pc_fantino_id=c.fantino_id
 inner join cavalli d on a.pc_cavallo_id=d.cavallo_id
 where pc_palio_id=19880702
 order by pc_canape
+
+-- Esordio Fantini
+select *
+from fantini f
+inner join palii_contrade pc
+    on f.fantino_id=pc.pc_fantino_id
+    and pc.pc_palio_id=f_fantino_esordio(f.fantino_id)
+order by pc_palio_id desc
